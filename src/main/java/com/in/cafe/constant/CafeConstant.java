754819a71a9a444c5596d05c112a248046b2b736
Vishal-PR <vishalpippirishetti@gmail.com>
2022-12-09 15:13:33 +0530

    Signup is created

---
+++ src/main/java/com/in/cafe/constant/CafeConstant.java
@@ -0,0 +1,8 @@
+package com.in.cafe.constant;
+
+public class CafeConstant {
+
+    public static final String SOMETHING_WENT_WRONG="Something Went Wrong.";
+
+    public static final String INVALID_DATA="Invalid Data.";
+}
